7c965523ce05f98babc7b8e01cdd97448397d4b5
EC2 Default User <ec2-user@ip-172-31-23-70.us-east-2.compute.internal>
2020-06-24 01:47:49 +0000

    mM

---
+++ resources/views/twitter/prof.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>テスト</title>
+        <link href="{{ secure_asset('css/prof.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <img src="{{ asset('img/prof_top.jpg') }}"></img>
+        <img src="{{ asset('img/long_prof.jpg') }}"></img>
+        <img src="{{ asset('img/prof_message.png') }}"></img>
+        <img src="{{ asset('img/prof_bottom.jpg') }}"></img>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('prof', function () {
+    return view('twitter.prof');
 });
