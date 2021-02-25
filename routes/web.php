321cbf7e4b88098dc6b91a8806cb28c821aba38d
EC2 Default User <ec2-user@ip-172-31-23-70.us-east-2.compute.internal>
2021-02-25 07:24:51 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//獲得用
+Route::get('app1', function () {
+    return view('fb.check'); //提出用
+    // return redirect('https://a-trade.jp/redirect/biscor?media=G16531'); //ビスコール - Aトレード
+});
+
+//確認用
+Route::get('cccheck', function () {
+    return redirect('https://a-trade.jp/redirect/biscor?media=G16531'); //ビスコール - Aトレード
+});
 
 // Twitter詳細プロフページ
 Route::get('prof', 'AdController@prf');
 
-// OS判定 → 遷移
+// OS判定 → 遷移 ※TikTok集客で使用中！
 Route::get('dl', 'AdController@os');
