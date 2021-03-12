65f6af6d3f0ee772cee8547bfa28efac3bc24a93
EC2 Default User <ec2-user@ip-172-31-23-70.us-east-2.compute.internal>
2021-03-12 21:58:47 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@
 //獲得用
 Route::get('app1', function () {
     return view('fb.check'); //提出用
-    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21252'); //友達追加リダイレクト - iOS
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21253'); //直友達追加リダイレクト - iOS
     
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21252'); //友達追加リダイレクト - iOS
     // return view('fb.adult'); //アダルト - LINE@誘導
     // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
     // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
@@ -23,6 +24,8 @@ Route::get('app1', function () {
 //獲得用
 Route::get('app11', function () {
     return view('fb.check'); //提出用
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21193'); //直友達追加リダイレクト - And
+    
     // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=21192'); //友達追加リダイレクト - And
 });
 
